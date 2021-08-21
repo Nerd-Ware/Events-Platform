c192885c29c372b46f6d1c16b95a38f30b715c62
Halakhamayseh <khamaysehhala95@yahoo.com>
2021-08-21 12:54:46 +0300

    userRelation

--- src/main/java/com/example/eventplatform/controller/UserController.java
+++ src/main/java/com/example/eventplatform/controller/UserController.java
@@ -5,10 +5,14 @@ import com.example.eventplatform.repository.DbUserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.servlet.view.RedirectView;
+
+import java.security.Principal;
 
 @Controller
 public class UserController {
@@ -43,5 +47,38 @@ public class UserController {
 
         dbUserRepository.save(newUser);
         return new RedirectView("/login");
+    }
+
+    @GetMapping("/profile")
+    public String profilepage(Principal p, Model model) {
+        DbUser me=dbUserRepository.findByUsername(p.getName());
+        model.addAttribute("me",me);
+        return "profile.html";
+    }
+
+    @GetMapping("/user/{id}")
+    public String profileIdpage( Model model, @PathVariable("id") int id,Principal p) {
+        DbUser user=dbUserRepository.findById(id).get();
+        model.addAttribute("user",user);
+        DbUser me=dbUserRepository.findByUsername(p.getName());
+
+        if (dbUserRepository.findByUsername(p.getName()).getFollowing().contains(user) || user == me){
+            model.addAttribute("status",false);
+        }else {
+            model.addAttribute("status",true);
+        }
+
+        return "user.html";
+    }
+
+    @PostMapping("/user/{id}")
+    public RedirectView addUser(@PathVariable("id") int id ,Principal p){
+        DbUser me=dbUserRepository.findByUsername(p.getName());
+        DbUser user=dbUserRepository.findById(id).get();
+        me.getFollowing().add(user);
+        user.getFollowers().add(me);
+        dbUserRepository.save(me);
+        dbUserRepository.save(user);
+        return new RedirectView("/user/{id}");
     }
 }
